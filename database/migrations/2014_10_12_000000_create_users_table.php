17287a4b772c1fa9a850681487b6a8c51524a399
isharaLiyanage <ishandananjaya1111@gmail.com>
2022-10-26 21:49:08 +0530

    fix err

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default(USER);
+            $table->string('role')->default(ADMIN);
             $table->string("image");
             $table->rememberToken();
             $table->timestamps();
