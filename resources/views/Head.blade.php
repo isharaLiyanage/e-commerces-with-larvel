b7263a013783c1afd99a4fbffcd0eb33d011ea7a
isharaLiyanage <ishandananjaya1111@gmail.com>
2022-11-03 11:03:27 +0530

    css up

--- resources/views/Head.blade.php
+++ resources/views/Head.blade.php
@@ -14,7 +14,7 @@ $total= ProductsController::cartItem();
    
     <!-- Styles -->
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
-    <link rel="stylesheet" href="{{ secure_asset('sass/app.scss') }}">
+   
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
         <!-- Fonts -->
@@ -150,6 +150,6 @@ $total= ProductsController::cartItem();
        
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
        
-     <script src="{{ secure_asset('js/app.js') }}"></script>
+
     </body>
 </html>
